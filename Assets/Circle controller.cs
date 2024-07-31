e9b3c1a8e32de010fbbb0cee7a58f3b52a0db966
BungusUngus <xavier.katzakis@hotmail.com>
2024-07-31 16:29:59 +1000

    new stuff added

--- Assets/Circle controller.cs
+++ Assets/Circle controller.cs
@@ -1,17 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Circlecontroller : MonoBehaviour
 {
     float speed = 200f;
-    float jumpSpeed = 400f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float jumpSpeed = 400f;    
 
     // Update is called once per frame
     void Update()
@@ -20,13 +14,13 @@ public class Circlecontroller : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             Rigidbody2D rb = GetComponent<Rigidbody2D>();
-            rb.AddForce(Vector2.right * 20f * Time.deltaTime);
+            rb.AddForce(Vector2.right * 80f * Time.deltaTime);
         }
 
         else if (Input.GetKey(KeyCode.A))
         {
             Rigidbody2D rb = GetComponent<Rigidbody2D>();
-            rb.AddForce(Vector2.left * 20f * Time.deltaTime);
+            rb.AddForce(Vector2.left * 80f * Time.deltaTime);
         }
         else
         {
